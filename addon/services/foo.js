9a3f5351cd3a296fbc94bc326bd8dc5b62dba2b4
Krati Ahuja <kratiahuja@gmail.com>
2020-11-02 23:20:54 -0800

    New service API

--- addon/services/foo.js
+++ addon/services/foo.js
@@ -1,7 +1,14 @@
 import Service from '@ember/service';
+import { inject as service } from '@ember/service';
 
 export default class FooService extends Service {
+  @service('bar') barManager;
+
   fooFunc() {
     console.log('I am being called from addon-test-a');
+  }
+
+  testFunc() {
+    this.barManager.newFunc();
   }
 }
